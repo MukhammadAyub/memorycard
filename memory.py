320c4543e6808fdb4486bd7e08aeeff3aa540cdb
MukhammadAyub <47212753+MukhammadAyub@users.noreply.github.com>
2022-08-19 10:08:14 +0500

    Update memory.py
    version 2.0

--- memory.py
+++ memory.py
@@ -22,3 +22,138 @@ main_win = QWidget()
 
 btn_ok = QPushButton('Ответить')
 question = QLabel()
+
+RadioGroupBox = QGroupBox('Варианты ответов:')
+rbtn_1 = QRadioButton()
+rbtn_2 = QRadioButton()
+rbtn_3 = QRadioButton()
+rbtn_4 = QRadioButton()
+
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+row1 = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+
+col1.addWidget(rbtn_1)
+col1.addWidget(rbtn_2)
+col2.addWidget(rbtn_3)
+col2.addWidget(rbtn_4)
+
+row1.addLayout(col1)
+row1.addLayout(col2)
+
+RadioGroupBox.setLayout(row1)
+
+AnsGroupBox = QGroupBox('Результаn теста')
+lb_result = QLabel()
+lb_correct = QLabel()
+
+layout_res = QHBoxLayout()
+layout_res.addWidget(lb_result, alignment=Qt.AlignLeft | Qt.AlignTop)
+layout_res.addWidget(lb_correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+row2 = QHBoxLayout()
+row2.addWidget(question, alignment=Qt.AlignHCenter)
+row3 = QHBoxLayout()
+row3.addStretch(1)
+row3.addWidget(btn_ok, stretch=2)
+row3.addStretch(1)
+row4 = QHBoxLayout()
+row4.addWidget(RadioGroupBox)
+row4.addWidget(AnsGroupBox)
+AnsGroupBox.hide()
+
+col3 = QVBoxLayout()
+col3.addLayout(row2, stretch=2)
+col3.addLayout(row4, stretch=8)
+col3.addLayout(row3, stretch=4)
+col3.addStretch(1)
+col3.setSpacing(5)
+
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_ok.setText('Следующий вопрос')
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_ok.setText('Ответить')
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)
+
+def test():
+    if btn_ok.text() ==  'Ответить':
+        show_result()
+    else:
+        show_question()
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+def ask(q):
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    question.setText(q.question)
+    lb_correct.setText(q.right_answer)
+    show_question()
+
+def show_correct(res):
+    lb_result.setText(res)
+    show_result()
+
+def check_answer():
+    if answers[0].isChecked():
+        show_correct('Правильно!')
+        main_win.score += 1
+        print('Статисктика:\n Всего вопросов задано:', main_win.total, '\Верных ответов:', main_win.score)
+        print('Рейтинг:\n', main_win.score/main_win.total*100)
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correct('Не правильно!')
+            print('Статисктика:\n Всего вопросов задано:', main_win.total, '\Верных ответов:', main_win.score)
+            print('Статисктика:\n', main_win.score/main_win.total*100)
+
+def next_question():
+    # main_win.cur_question += 1
+    main_win.total += 1
+    print('Статисктика:\n Всего вопросов задано:', main_win.total, '\Верных ответов:', main_win.score)
+    cur_question = randint(0, len(question_list) - 1 )
+    # if main_win.cur_question == len(question_list):
+    #     main_win.cur_question = 0
+    q = question_list[cur_question]
+    ask(q)
+
+def click_OK():
+    if btn_ok.text() == 'Ответить':
+        check_answer()
+    else:
+        next_question()
+
+q = Question('В каком году родился Аль-Хорезми?', '783', '971', '865', '1995')
+
+# main_win.cur_question = -1
+main_win.total = 0
+main_win.score = 0
+next_question()
+main_win.setLayout(col3)
+btn_ok.clicked.connect(click_OK)
+main_win.show()
+app.exec_()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
